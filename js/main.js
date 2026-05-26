b5cfbb30292f182f480ad51573b82c37aac8dcf4
w3sleygessner <w3sleygessner@gmail.com>
2026-05-26 15:51:25 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -337,3 +337,11 @@ window.excluirEmMassa = function() {
         UI.updateDashboard();
     });
 };
+
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').catch(err => {
+            console.log('Service Worker falhou:', err);
+        });
+    });
+}
